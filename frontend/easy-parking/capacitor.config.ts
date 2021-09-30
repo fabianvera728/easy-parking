c9728e5ba58b3e59cc61b2396bafda75ed4261e8
Fabian Vera <fabianvera728@gmail.com>
2021-09-29 21:11:01 -0500

    first commit

---
+++ frontend/easy-parking/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'easy-parking',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
