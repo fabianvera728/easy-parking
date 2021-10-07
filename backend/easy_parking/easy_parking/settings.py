49bd70d2de6fda8ab8210dc0657c460ad946cd2c
Fabian Vera <fabianvera728@gmail.com>
2021-10-07 14:25:23 -0500

    [add] cors url

--- backend/easy_parking/easy_parking/settings.py
+++ backend/easy_parking/easy_parking/settings.py
@@ -66,9 +66,11 @@ CORS_ALLOW_ALL_ORIGINS = True  # If this is used then `CORS_ALLOWED_ORIGINS` wil
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:4200',
+    'http://localhost:8100',
 ]  # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
 CORS_ALLOWED_ORIGIN_REGEXES = [
     'http://localhost:4200',
+    'http://localhost:8100',
 ]
 
 ROOT_URLCONF = 'easy_parking.urls'
