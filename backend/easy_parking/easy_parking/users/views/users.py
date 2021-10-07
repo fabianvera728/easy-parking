9b6ade0a0163fe60819fff92a46de96a966384bc
WilmerRS <estwilmerrodriguez@gmail.com>
2021-10-07 14:39:26 -0500

    [add] get vehicles by user

--- backend/easy_parking/easy_parking/users/views/users.py
+++ backend/easy_parking/easy_parking/users/views/users.py
@@ -6,11 +6,12 @@ from rest_framework.decorators import action
 # Models
 from easy_parking.users.models.profiles import Profile
 from easy_parking.reservations.models.reservations import Reservation
+from easy_parking.users.models.vehicles import Vehicle
 
 # Serializers
 from easy_parking.users.serializers.profile import ProfileSerializer
 from easy_parking.reservations.serializers.reservations import ReservationSerializer
-
+from easy_parking.users.serializers.vehicles import VehicleSerializer
 
 class Users(mixins.CreateModelMixin,
             mixins.RetrieveModelMixin,
@@ -25,6 +26,12 @@ class Users(mixins.CreateModelMixin,
     def reservations(self, request, *args, **kwargs):
         reservations = Reservation.objects.filter(vehicle__owner__user=self.get_object())
         serializer = ReservationSerializer(reservations, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['get'])
+    def vehicles(self, request, *args, **kwargs):
+        vehicles = Vehicle.objects.filter(owner=self.get_object())
+        serializer = VehicleSerializer(vehicles, many=True)
         return Response(serializer.data)
 
 # class ListUsers(APIView):
